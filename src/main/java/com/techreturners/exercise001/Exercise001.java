b9c89e34c0213c69cf76cddb45a7bc3559546690
shinesha <jodrew_7@hotmail.com>
2021-02-21 15:18:33 +0000

    updated after review

--- src/main/java/com/techreturners/exercise001/Exercise001.java
+++ src/main/java/com/techreturners/exercise001/Exercise001.java
@@ -2,44 +2,30 @@ package com.techreturners.exercise001;
 
 import java.util.List;
 import java.lang.*;
-import java.io.*;
-import java.util.*;
+
 
 public class Exercise001 {
     public String capitalizeWord(String word) {
-        // Add your code here
-        
         return word.substring(0,1).toUpperCase()+ word.substring(1) ;
     }
 
     public String generateInitials(String firstName, String lastName) {
-        // Add your code here
         return firstName.substring(0,1)+ "." +lastName.substring(0,1);
     }
 
     public double addVat(double originalPrice, double vatRate) {
-        // Add your code here
         double calc = ((originalPrice/100)*vatRate)+originalPrice;
         return Math.round(calc*100.0)/100.0;
     }
 
     public String reverse(String sentence) {
-        // Add your code here
         StringBuffer sbr = new StringBuffer(sentence);
-
         sbr.reverse();
         
         return sbr.toString();
     }
 
     public int countLinuxUsers(List<User> users) {
-        // Add your code here
-       // int occur = Collections.frequency(users, "Linux");
-        long count = users.stream().filter(x->x.getType().equals("Linux")).count();
-       
-        int count1 = (int)count;
-
-
-        return count1;
+        return (int)users.stream().filter(user->user.getType().equals("Linux")).count();
     }
 }
--- src/main/java/com/techreturners/exercise002/Exercise002.java
+++ src/main/java/com/techreturners/exercise002/Exercise002.java
@@ -1,23 +1,14 @@
 package com.techreturners.exercise002;
-import com.techreturners.exercise002.Person;
 
 public class Exercise002 {
 
     public boolean isFromManchester(Person person ) {
-
-        if (person.getCity().equals("Manchester"))
-
-        return true;
-        return false;
+        return (person.getCity().equals("Manchester"));
 
 }
 
     public boolean canWatchFilm(Person person, int ageLimit) {
-        // Add your code here
-        if (person.getAge()<ageLimit)
-
-        return false;
-        return true;
+        return (person.getAge()<ageLimit);
     }
     
 }
